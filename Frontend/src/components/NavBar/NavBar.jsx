802f3110c04936d9b5592d2bd219e8c0d86b3812
SohailShafiq1 <Sohail.shafiq002@gmail.com>
2025-07-22 01:11:48 +0500

    navbar setting

--- Frontend/src/components/NavBar/NavBar.jsx
+++ Frontend/src/components/NavBar/NavBar.jsx
@@ -1,25 +1,45 @@
+// NavBar.js
 import React from "react";
 import style from "./NavBar.module.css";
 import logo from "../../assets/logo.jpg";
 import { NavLink } from "react-router-dom";
+import { FiPhoneCall } from "react-icons/fi";
+import { BsSearch, BsSun } from "react-icons/bs";
+
 const s = style;
+
 const NavBar = () => {
   return (
     <div className={s.navBar}>
       <div className={s.logo}>
-        <img src={logo} alt="" />
+        <img src={logo} alt="Fresh Berry Logo" />
       </div>
-      <div className={s.search}>
-        <input type="text" placeholder="Type here" />
+
+      <div className={s.searchWrapper}>
+        <input type="text" placeholder="Type to search" />
+        <button><BsSearch /></button>
       </div>
-      <div className={s.btn}>
+
+      <div className={s.btns}>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/products">Products</NavLink>
-        <NavLink to="/horeca-supply">HoReCA Supply</NavLink>
-        <NavLink to="/qoute">Request Qoute</NavLink>
+        <NavLink to="/horeca-supply">HoReCa Supply</NavLink>
+        <NavLink to="/quote">Request a Quote</NavLink>
         <NavLink to="/about">About Us</NavLink>
       </div>
-      <div className={s.contact}></div>
+
+    <div className={s.con}>
+
+      <div className={s.toggle}>
+        <BsSun />
+      </div>
+
+      <div className={s.contact}>
+        <button>
+          <FiPhoneCall /> Contact Us
+        </button>
+      </div>
+    </div>
     </div>
   );
 };
