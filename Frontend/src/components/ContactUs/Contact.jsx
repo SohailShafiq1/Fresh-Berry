0d94e18e38532c57bc50a1e314a76fc63a5945ce
Israr Ahmad <asrarking831.aa@gmail.com>
2025-07-22 02:16:47 +0500

    added contact page

--- Frontend/src/components/ContactUs/Contact.jsx
+++ Frontend/src/components/ContactUs/Contact.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+import React from "react";
+import styles from "./Contact.module.css";
+import Bag from "../../assets/Bag.png";
+const s = styles;
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact Us</h1>
-      <p>If you have any questions, feel free to reach out!</p> 
-    </div>
-  )
-}
+    <>
+      <div className={s.container}>
+        <div className={s.topBox}>
+          <div className={s.topLeft}>
+            <h1>We're Here to Help You</h1>
+            <p>
+              Have questions or ready to start your fresh produce journey with
+              Fresh Berry? Reach out to our team today — we're happy to assist!
+            </p>
+            <div className={s.topButtons}>
+              <button className={s.button}>
+                <BsWhatsapp /> Whatsapp
+              </button>
+              <button className={s.button}>
+                <BsFillTelephoneFill /> Call Now
+              </button>
+            </div>
+          </div>
+          <div className={s.topRight}>
+            <img src={Bag} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
