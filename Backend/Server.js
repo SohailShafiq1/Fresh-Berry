3c589365b433b86973ab5549ad1f5026fabc3ae4
SohailShafiq1 <Sohail.shafiq002@gmail.com>
2025-07-26 02:42:12 +0500

    added netlify link

--- Backend/Server.js
+++ Backend/Server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://freshberryuae.com"],
+    origin: ["http://localhost:5173", "https://freshberryuae.com","https://freshberryproject.netlify.app"],
     credentials: true,
   })
 );
