f8edc4ba54c63f57e0b861943755bcb00919fa71
SohailShafiq1 <Sohail.shafiq002@gmail.com>
2025-08-17 20:28:19 +0500

    sticky nav bar added

--- Frontend/src/components/NavBar/NavBar.jsx
+++ Frontend/src/components/NavBar/NavBar.jsx
@@ -3,7 +3,8 @@ import style from "./NavBar.module.css";
 import logo from "../../assets/logo.jpg";
 import { NavLink, useNavigate } from "react-router-dom";
 import { FiPhoneCall, FiMenu, FiX } from "react-icons/fi";
-import { BsSearch, BsSun, BsMoon } from "react-icons/bs";
+import { BsSearch, BsSun, BsMoon, BsWhatsapp } from "react-icons/bs";
+import { MdEmail, MdPhone } from "react-icons/md";
 import { ThemeContext } from "../../context/Theme/ThemeContext";
 
 const s = style;
@@ -46,7 +47,35 @@ const NavBar = () => {
   };
 
   return (
-    <div className={s.navBar} data-theme={theme.text}>
+    <>
+      {/* Top Contact Bar */}
+      <div className={s.topBar} data-theme={theme.text}>
+        <div className={s.topBarContent}>
+          <div className={s.contactInfo}>
+            <a href="mailto:info@freshberryuae.com" className={s.emailLink}>
+              <MdEmail />
+              <span>info@freshberryuae.com</span>
+            </a>
+            <div className={s.phoneLinks}>
+              <a href="tel:+971585878022" className={s.phoneLink}>
+                <MdPhone />
+                <span>+971 58 587 8022</span>
+              </a>
+              <a 
+                href="https://wa.me/971585878022" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className={s.whatsappLink}
+              >
+                <BsWhatsapp />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Main Navbar */}
+      <div className={s.navBar} data-theme={theme.text}>
       <NavLink to="/" className={s.logo} onClick={() => setMenuOpen(false)}>
         <img src={logo} alt="Fresh Berry Logo" />
       </NavLink>
@@ -116,7 +145,8 @@ const NavBar = () => {
           {menuOpen ? <FiX size={24} /> : <FiMenu size={24} />}
         </div>
       </div>
-    </div>
+      </div>
+    </>
   );
 };
 
